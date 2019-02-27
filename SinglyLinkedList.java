34b79b527620a1e9e054f00b16e26d8e51265c40
hungryglobe8 <46876520+hungryglobe8@users.noreply.github.com>
2019-02-27 15:52:25 -0700

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -3,127 +3,149 @@ package assign06;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class SinglyLinkedList <E> implements List<E>{
-	
-	private class Node <E> {
+/**
+ * This class represents a SinglyLinkedList and implements from List
+ * @author Casey Rand and Morgan Mischo
+ *
+ * @param <E>
+ */
+public class SinglyLinkedList<E> implements List<E> {
+
+	private class Node<E> {
 		public E data;
-		public Node next;
+		public Node<E> next;
 		
-		public Node(E data, Node next)
+		public Node(E data, Node<E> next)
 		{
-			this.data = data; 
-			this.next = next; 
+			this.data = data;
+			this.next = next;
 		}
 	}
 	
-	private Node head; 
-	private int size; 
-	private Node <E> current;  
-	private Node <E> prev;
-	private Node <E> next;
+	private Node<E> head, last;
+	private int size;
 	
-	public SinglyLinkedList () {
+	public SinglyLinkedList()
+	{
 		head = null;
+		last = null;
 		size = 0;
-		
 	}
-
+	
 	@Override
 	public void addFirst(E element) {
 		if(head == null)
 		{
-			head = new Node (element, next);  
-			size++; 	
+			head = new Node<E> (element, last);
 		}
-		head.next = head;
-		head = new Node (element, next);
+		else
+		{
+			head = new Node<E> (element, head);
+		}
+		size++;
 	}
 
 	@Override
 	public void add(int index, E element) throws IndexOutOfBoundsException {
-		if (index > size)
+		if (index < 0 || index > size)
 		{
 			throw new IndexOutOfBoundsException (); 
 		}
 		
-		for (int i = 0; i <= size; i++)
+		//Index 0 acts as add first.
+		if (index == 0)
+		{
+			addFirst(element);
+			return;
+		}
+		
+		Node<E> current = head;
+		for (int i = 1; i <= size; i++)
 		{
 			if (i == index)
 			{	
-				Node temp = new Node (element, current.next);
+				Node<E> temp = new Node<E> (element, current.next);
 				current.next = temp; 
 				break; 
 			}
+			current = current.next;
 		}
+		size++;
 	}
 
 	@Override
 	public E getFirst() throws NoSuchElementException {
 		if (head.data == null)
 		{
-			throw new NoSuchElementException (); 
+			throw new NoSuchElementException();
 		}
 		
-		//Added a cast to E, but it should already be E (???!)
-		return (E) head.data; 
+		return head.data;
 	}
 
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
-		if (index > size)
+		if (index < 0 || index >= size)
 		{
-			throw new IndexOutOfBoundsException(); 
+			throw new IndexOutOfBoundsException (); 
 		}
-		int count = 0; 
 		
-		while(count <= size)
+		Node<E> current = head;
+		for (int i = 0; i <= size; i++)
 		{
-			if (index == count)
-			{
+			if (i == index)
+			{	
 				return current.data; 
 			}
-			count++;
-			current = current.next; 
+			current = current.next;
 		}
-		return null; 
+		return null;
 	}
 
 	@Override
 	public E removeFirst() throws NoSuchElementException {
-		E result = (E) head.data; 
+		if (head == null)
+		{
+			throw new NoSuchElementException();
+		}
+		
+		E result = head.data; 
 		
 		head = head.next; 
-		
-		return result; 
+		size--;
+		return result; 		
 	}
 
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
-		
-		if (index > size)
+		if (index < 0 || index >= size)
 		{
 			throw new IndexOutOfBoundsException(); 
 		}
-		int count = 0; 
 		
-		while(count <= size)
+		Node<E> current = head;
+		Node<E> prev = null;
+		
+		for (int i = 0; i <= size; i++)
 		{
-			if (index == count)
-			{
+			if (i == index)
+			{	
 				prev.next = current.next;
-				return current.data; 
+				size--;
+				break;
 			}
-			count++; 
-			current = current.next; 
+			prev = current;
+			current = current.next;
 		}
-		return null; 
+		return current.data;
 	}
 
 	@Override
 	public int indexOf(E element) {
 		int count = 0; 
 		
-		while(count <= size)
+		Node<E> current = head;
+		while(count < size)
 		{
 			if (current.data == element)
 			{
@@ -137,12 +159,12 @@ public class SinglyLinkedList <E> implements List<E>{
 
 	@Override
 	public int size() {
-		return size; 
+		return size;
 	}
 
 	@Override
 	public boolean isEmpty() {
-		if (size >= 0)
+		if (size > 0)
 		{
 			return false; 
 		}
@@ -153,21 +175,32 @@ public class SinglyLinkedList <E> implements List<E>{
 	public void clear() {
 		size = 0; 
 		head = null; 
-		
 	}
 
 	@Override
 	public Object[] toArray() {
-		//Error creating E[]
 		Object[] temp = new Object[size]; 
 		
-		for (int i = 0; i <= size; i++)
+		Node<E> current = head;
+		for (int i = 0; i < size; i++)
 		{
-			temp[i] = current.data; 
+			temp[i] = current.data;
+			current = current.next;
 		}
 		return temp; 
 	}
-
+	
+//	public String toString() {
+//		String temp = "";
+//		Node<E> current = head;
+//		for (int i = 0; i < size; i++)
+//		{
+//			temp = temp + current.data + " ";
+//			current = current.next;
+//		}
+//		return temp;
+//	}
+	
 	@Override
 	public Iterator <E> iterator() {
 		return new SinglyLinkedListIterator(); 
@@ -218,8 +251,6 @@ public class SinglyLinkedList <E> implements List<E>{
 				prev.next = next; 
 			}
 			removable = false; 
-			
-			}
-		
+		}
 	}
 }
